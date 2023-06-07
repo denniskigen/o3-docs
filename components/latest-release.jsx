6728d9d60738d7abe9831a927d0636681e49f714
Dennis Kigen <kigen.work@gmail.com>
2023-06-07 20:53:55 +0300

    Tweaks

--- components/latest-release.jsx
+++ components/latest-release.jsx
@@ -1,28 +1,30 @@
-import useSWR from "swr";
+import useSWRImmutable from "swr";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function LatestRelease({ repo }) {
   const fetcher = (url) => fetch(url).then((r) => r.json());
   const apiUrl = `https://api.github.com/repos/openmrs/${repo}/releases`;
   const repoUrl = `https://github.com/openmrs/${repo}/releases`;
+  const { data, error } = useSWRImmutable(apiUrl, fetcher);
 
-  const { data, error } = useSWR(apiUrl, fetcher);
-
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-  if (data.length === 0) return <div className="py-5">No releases found.</div>;
+  if (error) return <div className="py-4">Failed to load</div>;
+  if (!data) return <div className="py-4">Loading...</div>;
+  if (data.length === 0) return <div className="py-4">No releases found.</div>;
 
   const latestRelease = data && data[0];
 
   return (
     <div className="py-4 space-y-2">
       <h1 className="font-bold text-lg">{latestRelease?.name}</h1>
-      <ReactMarkdown className="markdown">{latestRelease?.body}</ReactMarkdown>
+      <ReactMarkdown className="markdown" remarkPlugins={[remarkGfm]}>
+        {latestRelease?.body}
+      </ReactMarkdown>
 
       {data?.length > 1 ? (
-        <h1>
+        <h1 className="py-2">
           <a
-            className="text-blue-600 underline"
+            className="text-sky-500 underline"
             href={repoUrl}
             rel="noopener noreferrer"
             target="_blank"

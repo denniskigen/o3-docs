0c2dff65996d5be7785aaab95b3d99e2d78bc3a7
Dennis Kigen <kigen.work@gmail.com>
2024-04-17 14:15:32 +0300

    (feat) Add framework API reference to docs (#30)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./components/**/*.jsx",
-    "./pages/**/*.{md,mdx}",
-    "./theme.config.tsx",
-  ],
+  content: ["./components/**/*.jsx", "./pages/**/*.{md,mdx}", "./theme.config.tsx"],
+  plugins: [require("@tailwindcss/typography")],
 };
